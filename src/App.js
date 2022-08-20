465d59f8ab856e96d992b83de04a369b1b21722e
tbrsi <99534701+tbrsi@users.noreply.github.com>
2022-08-20 19:11:22 +0430

    add axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
+
 import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather
-        city={"NewYork"}
-        CurrentTemp={19}
-        CurrentSky={"Cloudy"}
-        WindSpeed={10}
-        MaxTemp={15}
-        MinTemp={8}
-        CurrentDate={"Tuesday 10:00"}
-      />
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
-export default function Weather(props) {
+export default function Weather() {
+  const [city, setCity] = useState("tehran");
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    setWeatherData({
+      city: response.data.name,
+      currentTemp: Math.round(response.data.main.temp),
+      description: response.data.weather[0].main,
+      maxTemp: Math.round(response.data.main.temp_max),
+      minTemp: Math.round(response.data.main.temp_min),
+      wind: Math.round(response.data.wind.speed),
+    });
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "ad98cfa59bdce2d355527064e3835264";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div className="Weather">
       <div className="MainBorder">
-        <h1>{props.city}</h1>
+        <h1>{weatherData.city}</h1>
         <div className="row">
           <div className="col-6">
             <img
@@ -14,7 +36,7 @@ export default function Weather(props) {
               id="icon"
             />
             <span className="current-temp" id="current-temp">
-              {props.CurrentTemp}
+              {weatherData.currentTemp}
             </span>
             <span className="units">
               <a href="/" className="celecius">
@@ -29,9 +51,9 @@ export default function Weather(props) {
           </div>
           <div className="col-6">
             <ul className="weather-condition">
-              <li className="current-sky">{props.CurrentSky}</li>
+              <li className="current-sky">{weatherData.description}</li>
               <li className="wind-speed">
-                Wind: <span>{props.WindSpeed}</span> km/h
+                Wind: <span>{weatherData.wind}</span> km/h
               </li>
             </ul>
           </div>
@@ -39,26 +61,27 @@ export default function Weather(props) {
         <div className="row">
           <div className="col-6">
             <i className="fa-solid fa-caret-up"></i>
-            <span className="max-temp">{props.MaxTemp}</span>
+            <span className="max-temp">{weatherData.maxTemp}</span>
             <span className="degree-sign">°</span>
             <i className="fa-solid fa-caret-down"></i>
-            <span className="min-temp">{props.MinTemp}</span>
+            <span className="min-temp">{weatherData.minTemp}</span>
             <span className="degree-sign">°</span>
           </div>
           <div className="col-6">
-            <span className="current-date">{props.CurrentDate}</span>
+            <span className="current-date">{"saturday 10:00 pm"}</span>
           </div>
         </div>
         <hr />
-        <form className="search-form">
+        <form className="search-form" onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-6">
               <input
                 type="search"
                 className="form-control"
                 placeholder="Enter a city"
-                autofocus="on"
-                autocomplete="off"
+                autoFocus="on"
+                autoComplete="off"
+                onChange={updateCity}
               />
             </div>
             <div className="col-3">
